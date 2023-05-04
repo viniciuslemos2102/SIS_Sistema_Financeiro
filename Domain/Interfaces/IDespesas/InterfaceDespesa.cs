eaad03febb61440b4b6a40a661070b0dfc6bb5a7
viniciuslemos2102 <viniciuslemoscontato21@gmail.com>
2023-05-04 17:16:59 -0300

    Respositorios Iniciados

---
+++ Domain/Interfaces/IDespesas/InterfaceDespesa.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Interfaces.Generecs;
+using Entities.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.IDespesas
+{
+    public interface InterfaceDespesa : InterfaceGeneric<Despesa>
+    {
+        Task<IList<Despesa>> ListarDespesasUsuario(string emailUsuario);
+        Task<IList<Despesa>> ListarDespesasUsuarioNaoPagasMesesAnterior(string emailUsuario);
+    }
+}
