f010ff789339641fff57d7963d7f41e477cbdc89
viniciuslemos2102 <viniciuslemoscontato21@gmail.com>
2023-05-11 18:44:27 -0300

    colocando o autorazi nas controllers

--- WebApi/Controllers/SistemaFinanceiroController.cs
+++ WebApi/Controllers/SistemaFinanceiroController.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Interfaces.InterfaceServicos;
 using Domain.Interfaces.ISistemaFinanceiro;
 using Entities.Entidades;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,7 @@ namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class SistemaFinanceiroController : ControllerBase
     {
         private readonly InterfaceSistemaFinanceiro _interfaceSistemaFinanceiro;
